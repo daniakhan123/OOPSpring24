6186b543605a722ff6932d85e7a5159a5f1374fe
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-04-30 00:04:45 +0500

    Update q2.cpp

--- LAB 08/q2.cpp
+++ LAB 08/q2.cpp
@@ -1,52 +1,60 @@
-#include &lt;iostream&gt;
-#include &lt;string&gt;
+#include <iostream>
+#include <string>
 using namespace std;
+
 class Character {
 protected:
+    int health;
+    int damage;
 
-    int health;
-    int damage;
 public:
-    Character(int h, int d) : health(h), damage(d) {}
-    void display() const {
-        cout &lt;&lt; &quot;Health: &quot; &lt;&lt; health &lt;&lt; endl;
-        cout &lt;&lt; &quot;Damage: &quot; &lt;&lt; damage &lt;&lt; endl;
-    }
+    Character(int h, int d) : health(h), damage(d) {}
+
+    void display() const {
+        cout << "Health: " << health << endl;
+        cout << "Damage: " << damage << endl;
+    }
 };
+
 class Enemy : public Character {
 public:
-    Enemy(int h, int d) : Character(h, d) {}
-    void display() const {
-        cout &lt;&lt; &quot;Enemy Details:&quot; &lt;&lt; endl;
-        Character::display();
-    }
+    Enemy(int h, int d) : Character(h, d) {}
+
+    void display() const {
+        cout << "Enemy Details:" << endl;
+        Character::display();
+    }
 };
+
 class Player : public Character {
 public:
-    Player(int h, int d) : Character(h, d) {}
-    void display() const {
-        cout &lt;&lt; &quot;Player Details:&quot; &lt;&lt; endl;
-        Character::display();
-    }
+    Player(int h, int d) : Character(h, d) {}
+
+    void display() const {
+        cout << "Player Details:" << endl;
+        Character::display();
+    }
 };
+
 class Wizard : public Player {
 private:
-    int magicPower;
-    string spells;
-public:
-    Wizard(int h, int d, int mp, const string&amp; sp)
-        : Player(h, d), magicPower(mp), spells(sp) {}
-    void display() const {
-        cout &lt;&lt; &quot;Wizard Details:&quot; &lt;&lt; endl;
-        Player::display();
-        cout &lt;&lt; &quot;Magic Power: &quot; &lt;&lt; magicPower &lt;&lt; endl;
+    int magicPower;
+    string spells;
 
-        cout &lt;&lt; &quot;Spells: &quot; &lt;&lt; spells &lt;&lt; endl;
-    }
+public:
+    Wizard(int h, int d, int mp, const string& sp)
+        : Player(h, d), magicPower(mp), spells(sp) {}
+
+    void display() const {
+        cout << "Wizard Details:" << endl;
+        Player::display();
+        cout << "Magic Power: " << magicPower << endl;
+        cout << "Spells: " << spells << endl;
+    }
 };
+
 int main() {
- 
-    Wizard wizard(100, 20, 50, &quot;Fireball, Lightning Bolt&quot;);
-    wizard.display();
-    return 0;
+    Wizard wizard(100, 20, 50, "Fireball, Lightning Bolt");
+    wizard.display();
+    return 0;
 }
