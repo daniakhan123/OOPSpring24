445ac4d2745b22324e11bee02e27e986eeb1b2dc
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-04-30 00:05:29 +0500

    Update q1.cpp

--- LAB 08/q1.cpp
+++ LAB 08/q1.cpp
@@ -1,40 +1,42 @@
-#include &lt;iostream&gt;
-#include &lt;string&gt;
+#include <iostream>
+#include <string>
 using namespace std;
+
 class Book {
 protected:
-    string title;
-    string author;
-    string publisher;
+    string title;
+    string author;
+    string publisher;
+
 public:
-    Book(const string&amp; t, const string&amp; a, const string&amp; p)
-        : title(t), author(a), publisher(p) {}
-    void display() const {
-        cout &lt;&lt; &quot;Title: &quot; &lt;&lt; title &lt;&lt; endl;
-        cout &lt;&lt; &quot;Author: &quot; &lt;&lt; author &lt;&lt; endl;
-        cout &lt;&lt; &quot;Publisher: &quot; &lt;&lt; publisher &lt;&lt; endl;
-    }
+    Book(const string& t, const string& a, const string& p)
+        : title(t), author(a), publisher(p) {}
+
+    void display() const {
+        cout << "Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "Publisher: " << publisher << endl;
+    }
 };
+
 class FictionBook : public Book {
 private:
-    string genre;
-    string protagonist;
-public:
-    FictionBook(const string&amp; t, const string&amp; a, const string&amp; p, const string&amp;
-g, const string&amp; pro)
-        : Book(t, a, p), genre(g), protagonist(pro) {}
-    void display() const {
-        Book::display();
+    string genre;
+    string protagonist;
 
-        cout &lt;&lt; &quot;Genre: &quot; &lt;&lt; genre &lt;&lt; endl;
-        cout &lt;&lt; &quot;Protagonist: &quot; &lt;&lt; protagonist &lt;&lt; endl;
-    }
+public:
+    FictionBook(const string& t, const string& a, const string& p, const string& g, const string& pro)
+        : Book(t, a, p), genre(g), protagonist(pro) {}
+
+    void display() const {
+        Book::display();
+        cout << "Genre: " << genre << endl;
+        cout << "Protagonist: " << protagonist << endl;
+    }
 };
+
 int main() {
-   
-    FictionBook fiction(&quot;The Great Gatsby&quot;, &quot; Scott Fitzgerald&quot;, &quot;Scribner&quot;,
-&quot;Novel&quot;, &quot;Jay Gatsby&quot;);
-   
-    fiction.display();
-    return 0;
+    FictionBook fiction("The Great Gatsby", "Scott Fitzgerald", "Scribner", "Novel", "Jay Gatsby");
+    fiction.display();
+    return 0;
 }
