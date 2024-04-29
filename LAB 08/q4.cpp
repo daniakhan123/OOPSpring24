1286cdd296748a0d35b70b920f2e1e42f3e8f068
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-04-30 00:04:01 +0500

    Update q4.cpp

--- LAB 08/q4.cpp
+++ LAB 08/q4.cpp
@@ -1,42 +1,48 @@
-#include &lt;iostream&gt;
-#include &lt;string&gt;
+#include <iostream>
+#include <string>
 using namespace std;
+
 class Person {
 protected:
-    string name;
-    int age;
-public:
+    string name;
+    int age;
 
-    Person(const string&amp; n, int a) : name(n), age(a) {}
+public:
+    Person(const string& n, int a) : name(n), age(a) {}
 };
+
 class Student : public Person {
 private:
-    string student_id;
-    int grade_level;
+    string student_id;
+    int grade_level;
+
 public:
-    Student(const string&amp; n, int a, const string&amp; id, int grade) : Person(n, a),
-student_id(id), grade_level(grade) {}
-    void display() {
-        cout &lt;&lt; &quot;Student: &quot; &lt;&lt; name &lt;&lt; &quot;, Age: &quot; &lt;&lt; age &lt;&lt; &quot;, ID: &quot; &lt;&lt; student_id
-&lt;&lt; &quot;, Grade Level: &quot; &lt;&lt; grade_level &lt;&lt; endl;
-    }
+    Student(const string& n, int a, const string& id, int grade) : Person(n, a), student_id(id), grade_level(grade) {}
+
+    void display() {
+        cout << "Student: " << name << ", Age: " << age << ", ID: " << student_id << ", Grade Level: " << grade_level << endl;
+    }
 };
+
 class Teacher : public Person {
 private:
-    string subject;
-    string room_number;
+    string subject;
+    string room_number;
+
 public:
-    Teacher(const string&amp; n, int a, const string&amp; subj, const string&amp; room) :
-Person(n, a), subject(subj), room_number(room) {}
-    void display() {
-        cout &lt;&lt; &quot;Teacher: &quot; &lt;&lt; name &lt;&lt; &quot;, Age: &quot; &lt;&lt; age &lt;&lt; &quot;, Subject: &quot; &lt;&lt;
-subject &lt;&lt; &quot;, Room Number: &quot; &lt;&lt; room_number &lt;&lt; endl;
-    }
+    Teacher(const string& n, int a, const string& subj, const string& room) : Person(n, a), subject(subj), room_number(room) {}
+
+    void display() {
+        cout << "Teacher: " << name << ", Age: " << age << ", Subject: " << subject << ", Room Number: " << room_number << endl;
+    }
 };
+
 int main() {
-    Student student1(&quot;Dania&quot;, 15, &quot;S12345&quot;, 10);
-    Teacher teacher1(&quot;Sir Talha&quot;, 35, &quot;OOP&quot;, &quot;Room C05&quot;);
-    student1.display();
-    teacher1.display();
-    return 0;
+    Student student1("Dania", 15, "S12345", 10);
+    Teacher teacher1("Sir Talha", 35, "OOP", "Room C05");
+
+    student1.display();
+    teacher1.display();
+
+    return 0;
 }
